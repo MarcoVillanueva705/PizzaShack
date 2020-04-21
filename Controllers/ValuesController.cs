3fa8f8a481cae5290e96d0f67ad636a7b8e81e29
marco villanueva <villanuevamarco98@gmail.com>
2020-04-21 16:32:05 -0600

    testing get requests

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pizza_shack.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       public List<string> MyValues = new List<string>() {"value1", "value2", "value3"};
+
+    // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return MyValues;
+        }
+
+    //GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if(id < MyValues.Count)
+            {
+                return MyValues[id];
+            }
+            return BadRequest("Index out of range");
+        }
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+    }
+}
