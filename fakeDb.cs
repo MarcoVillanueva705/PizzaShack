6b09b3c8dfd9149f21b9d1a2fa704aaeb29c29d3
marco villanueva <villanuevamarco98@gmail.com>
2020-04-23 11:18:20 -0600

    fake db added

---
+++ fakeDb.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace pizza_shack.Mock
+{
+    public static class fakeDb
+    {
+        public static List<string> MyValues = new List<string>() {"value1", "value2", "value3"};
+
+    }
+}
