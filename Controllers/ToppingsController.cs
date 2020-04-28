8c02c8724c65d6e8b1a76435151934f2efe2a58c
marco villanueva <villanuevamarco98@gmail.com>
2020-04-28 17:11:39 -0600

    toppings controller avail toppings works

--- Controllers/ToppingsController.cs
+++ Controllers/ToppingsController.cs
@@ -1,17 +1,18 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using pizza_shack.Mock;
 using pizza_shack.Models;
 
 namespace pizza_shack.Controllers
 {
-    [Route("api/toppings")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ToppingsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<Topping>> Get()
+        public ActionResult<Dictionary<string, Topping>> Get()
         {
-            
+            return fakeDb.AvailToppings;
         }
     }
 }
