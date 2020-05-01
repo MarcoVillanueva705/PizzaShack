d52f75cea7ca99c3bc3fdca874565f36fd24d810
marco villanueva <villanuevamarco98@gmail.com>
2020-05-01 17:57:14 -0600

    added post for test to orders controllers

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -2,3 +2,23 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using pizza_shack.Mock;
 using pizza_shack.Models;
+
+namespace pizza_shack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> Get()
+        {
+            return fakeDb.Orders;
+        }
+
+        [HttpPost]
+        public ActionResult<Order> Create([FromBody] Order o)
+        {
+            return o;
+        }
+    }
+}
