0336579307f25ae11824931e866c41a6f2dc119d
marco villanueva <villanuevamarco98@gmail.com>
2020-05-02 08:56:37 -0600

    added order request to order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -3,6 +3,16 @@ using System.Linq;
 
 namespace pizza_shack.Models
 {
+    public class OrderRequest
+    {
+        List<Pizza> Pizzas { get; set; }
+
+        public OrderRequest()
+        {
+
+        }
+    
+    }
     public class Order
     {
         public List<Pizza> Pizzas { get; set; }
